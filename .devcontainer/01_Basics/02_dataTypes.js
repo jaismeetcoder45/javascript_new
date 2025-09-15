7b52e4b3f7328de58db18f736c672474d517ff58
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-09-15 20:01:48 +0530

    config done

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";//use all js code as newer version 
+
+// alert(3 + 3)//cant be used in node js 
+
+// console.log(3 + 3); console.log("jais")//dont do this as it wrongs the readability
+// code readability should be high 
+
+//write
+console.log(3+3)
+console.log("jais")
+// dc39.ecmascript read about 
+
+let name = "Jaismeet"
+let age = 18
+let isLoggedIn = false
+let state = null//null means it is empty
+let city;
+
+
+/*
+//data types:-
+numbers ==> 2 to the power 32
+bigint
+string ==> " " use double quotes
+boolean ==> false/true
+null ==> standalone value
+undefined
+symbol ==> unique
+
+*/
+//object
+console.log(typeof(undefined))
+console.log(typeof(null))
+// type of null is object
+// read about specifications
