b13f63e293005e109947869c33a44e3421c0f6d9
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-09-16 15:39:12 +0530

    done

---
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+// let score = "33abc"
+// let score = null//comes 0
+// let score = undefined//comes undefined
+// let score = true//comes undefined
+// let score = "jaismeet"//comes undefined
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valInNum = Number(score)//changing the data type
+// console.log(typeof valInNum);
+// console.log(valInNum);//NaN means not a number
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+// let isLoggedIn = "Jaismeet"
+
+// let booleanIsLoggendIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggendIn)
+// console.log(booleanIsLoggendIn)
+
+/*
+1 => true0 => false
+"" => false
+"jaismeet" => true
+*/
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
