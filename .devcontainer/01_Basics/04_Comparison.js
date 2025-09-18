da3083cf1d18bd269f473c206a7f3f5939e091f2
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-09-18 19:22:13 +0530

    done

---
+++ .devcontainer/01_Basics/04_Comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);//2 automatically converted to integer
+// console.log("02" > 1);
+// always make sure while comparison data types is same dont use different data types
+/*
+never use this logic. avoid these 
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)//not predictable result//null is converted to 0 in here in line 14
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined >= 0)
+*/
+// === strictly check the values (check data type as well)
+console.log("2" === 2)
