478ed0145d429d5f83ef58dacaceb1b906c094f6
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-10-09 14:42:04 +0530

    done

---
+++ .devcontainer/03_Basics/04_IIFE.js
@@ -0,0 +1,15 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+// function chai(){
+//     console.log(`DB CONNECTED`);   
+// }
+// chai()
+(function chai(){
+    console.log(`DB CONNECTED`);   
+})();   // -> //use semicolon her to avoid error in next function
+
+//first () for defination, second() for execution
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('Jais')
