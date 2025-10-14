b971f927f540b8db2234dd94f6012a7826aa0482
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-10-14 15:43:59 +0530

    done

---
+++ .devcontainer/05_iterations/02.js
@@ -0,0 +1,24 @@
+//while loop
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(`value of index is ${i}`);
+//     i = i + 2;
+// }
+
+// let arr = ["jais", "gunnu", "rajveer"]
+
+// let a = 0;
+// while (a<arr.length) {
+//     console.log(`value is ${arr[a]}`);
+//     a = a + 1
+// }
+
+//do while loop
+
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
