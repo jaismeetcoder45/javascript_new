c68e2ca21ba506936e7e1ac46d269c2a088c78b8
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-10-16 17:39:08 +0530

    yayyyy

---
+++ .devcontainer/05_iterations/04.js
@@ -0,0 +1,22 @@
+const obj = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "ruby"
+}
+
+// for (const key in obj) {
+//     console.log(`${key} shortcut is for ${obj[key]}`);
+// }
+const programming = ["js","rb","py","c++"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA', "United states of america")
+map.set('FR', "France")
+
+// for (const key in map) {
+//     console.log(key);//map is not iterable
+// }
---
+++ .devcontainer/05_iterations/05.js
@@ -0,0 +1,28 @@
+//for each loop
+
+const coding = ["jais","veer","harry","kostov"]//array
+
+// coding.forEach(function (item) {
+//     console.log(item);
+    
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function print(item){
+//     console.log(item);   
+// }
+// // coding.forEach(print)
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+    
+// })
+
+const myCoding = [
+    {
+        langName: "Js",
+        langFileName: "py"
+    }
+]
