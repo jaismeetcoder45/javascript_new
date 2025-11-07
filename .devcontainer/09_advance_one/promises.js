9333c4f0cf6fe6997b18f264089e1180d78c9631
Jaismeet Kaur <gstsinghhardeep@gmail.com>
2025-11-07 17:52:02 +0530

    lec40

--- .devcontainer/09_advance_one/promises.js
+++ .devcontainer/09_advance_one/promises.js
@@ -58,3 +58,50 @@ promise4.then((user) => {
     console.log(error);
 }).finally(() => {console.log("the promise is either resolved or rejected")}
 )
+
+const promise5 = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "javascript",passw: "123"})
+        }
+        else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+})
+
+//it works only if everything is going alright
+async function consumepromise5(){
+    try {
+        const response = await promise5
+        console.log(response);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+    //promise is an object so we store it in a variable
+}
+consumepromise5()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E:", error);
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
